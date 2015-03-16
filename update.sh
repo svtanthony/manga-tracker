9e60fc481a3b1843e02bef0775d11d244941d141
Roberto Pasillas <rpasi001@ucr.edu>
2015-03-16 16:20:08 -0700

    updated.sh

--- update.sh
+++ update.sh
@@ -1,45 +1,63 @@
 #/bin/bash
 #=============================================================================
 # Script Name:	update.sh
-# Date:			2/25/15
-# Purpose:		This script will receive a manga to check and check the links 
-#				associated with the manga to check if there's an update.
+# By:			Roberto A. Pasillas (svtanthony@gmail.com)
+# Date:			2/27/15
+# Purpose:		This script will send an email if any of the tracked manga have 
+#				updated and download the chapter if downloading is enabled. If a 
+#				link is passed in, it will download starting from the link to
+#				the most recent chapter.
 #=============================================================================
 # ********************   Config   ********************************************
+
+#email address to send updates to
 email="svtanthony@gmail.com"
-#=============================================================================
+
+#set to "true" to download new releases
+downEn="false"
+
+# UserAgent is required for some websites to allow connectivity
 userAgent='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4'
-# check tracked manga
-if [ -n "$1" ] # go into download mode or enable downloading with -D flag
+
+#=============================================================================
+cd ~/manga-tracker/
+
+if [ -n "$1" ] # check for updates or downlaod manga
 then
-
-	echo "input detected : $1"
-
-	if [ -n "$1" ] # check  if -D to enable downloading 
-	then
-		echo something	
-	fi
-fi
-
-sed 's/[a-z0-9\-\_\.]*@.*//' Contact | sort -u | while read mangaName
-do
-	latestChp=$(cat Links | grep $mangaName | awk -F ' ' '{print $2}')
-	#obtain link in a loop#####################################################################################################################
-	link=$(cat Links | grep $mangaName | awk -F ' ' '{print $3}')
-	parseName=$(cat Links | grep $mangaName | awk -F ' ' '{print $3}' | sed 's/.*\///g')
-	chp=$(wget -q -U $userAgent -O- $link | grep $parseName | head -5 | tail -1 | grep -oE c[0-9]\{1,4\} | sed 's/^c0\?//g')
+	echo "download script here"
+else
+	awk -F ' ' '{print $1}' Links | while read mangaName
+	do
+		grep $mangaName Links | cut -d " " -f 3- | sed 's/ /\n/g' | while read link
+		do 
+			latestChp=$(grep $link Links | awk -F ' ' '{print $2}')
+			parseName=$(echo $link | sed 's/.*\///g')
 	
-	echo latest chapter is $latestChp
-	echo link is $link
-	echo parse name = $parseName
-	echo chp is $chp
-	if [ "$chp" -gt "$latestChp" ]
-	then
-		echo "UPDATE FOUND"
-		#echo "The latest Chapter($chp) of $mangaName has been released" | mail -s "Manga Update" $email &
-	fi
-
-	echo
-
-done
-
+			# if to determine chp 
+			if (echo $link | grep -qiE '(mangahere|mangatown)')
+			then
+				chp=$(wget -O- -q -U $userAgent $link | grep $parseName | head -5 | tail -1 | grep -oE c[0-9]\{1,4\} | sed 's/^c0\?//g')
+			elif (echo $link | grep -qiE '(mangareader|mangapanda|mangastream)')
+			then
+				chp=$(wget -O- -q -U $userAgent $link | grep $parseName | head -2 | tail -1 | grep -oE [0-9]\{1,4\} | head -1)
+			elif (echo $link | grep -qiE '(mangacow)')
+			then
+				chp=$(wget -O- -q -U $userAgent $link | grep $parseName | head -5 | tail -1 | grep -oE [0-9]\{,4\} | head -1)
+			else
+				chp=$latestChp
+			fi
+			
+			# actions - Email and Download
+			if [ "$chp" -gt "$latestChp" ]
+			then
+				sed -i "s/$mangaName $latestChp/$mangaName $chp/" Links
+				echo "The latest Chapter($chp) of $mangaName has been released.  $link" | mail -s "Manga Update" $email &
+				if [ $downEn == "true" ]
+				then
+					echo extract appropriate link then pass it on.
+					#./update $link 
+				fi
+			fi
+		done
+	done
+fi
