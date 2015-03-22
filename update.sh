8452862b19a92a9e6f24095b0df3bcf418a16b1f
Roberto Pasillas <rpasi001@ucr.edu>
2015-03-21 21:19:19 -0700

    merged download and update scripts

--- update.sh
+++ update.sh
@@ -24,7 +24,104 @@ cd ~/manga-tracker/
 
 if [ -n "$1" ] # check for updates or downlaod manga
 then
-	echo "download script here"
+# ========================================= DOWNLOAD =====================================================================
+	# intial parameter passed to function
+	link=$1
+	userAgent="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4"
+
+	# obtain name of series & image url
+	if (echo $link | grep -qiE '(mangahere|mangatown)')
+	then
+		name=$(echo "$link" | sed -e 's/.*manga\///g' -e 's/\/.*//g')
+		download=$(wget -qO- -U $userAgent $link | grep .*.jpg | head -2 | tail -1 | grep -o http.*.jpg)
+	elif (echo $link | grep -qi 'mangapanda')
+	then
+		name=$(echo  $link | sed -e 's/.*m\///' -e 's/\/.*//')
+		download=$(wget -qO- $link | grep -o .*jpg | sed 's/.*src=\"//' | tail -1)
+	elif (echo $link | grep -qi 'mangareader')
+	then
+		name=$(echo  $link | sed -e 's/.*t\///' -e 's/\/.*//g')
+		download=$(wget -qO- $link | grep -o .*jpg | sed 's/.*src=\"//' | tail -1)
+	elif (echo $link | grep -qi 'mangacow')
+	then
+		name=$(echo  $link | sed -e 's/.*o\///' -e 's/\/.*//g')
+		download=$(wget -qO- $link | sed 's/;/\n/g'| grep -o .*jpg | sed 's/.*src=\"//' | tail -2 | head -1 )
+	fi
+
+	# cd to manga-tracker & create download directory
+	cd ~/manga-tracker/
+	if ( ! (ls ~/manga-tracker | grep -q $name) )
+	then
+		mkdir $name
+	fi
+	cd $name
+
+
+	while (test -n $link -a -n $download)
+	do
+	
+		#chapter variable for directory
+		if (echo $link | grep -qiE '(mangahere|mangatown)')
+		then
+			chp=$(echo "$link" | sed -e "s/.*$name\///g" -e 's/[a-zA-Z]*//g' -e 's/^0//g' -e 's/\/.*//g')
+		elif (echo $link | grep -qi 'mangapanda')
+		then
+			chp=$(echo  $link | sed -e "s/.*$name\///g" -e 's/\/.*//')
+		elif (echo $link | grep -qi 'mangareader')
+		then
+			chp=$(echo  $link | sed -e "s/.*$name\///g" -e 's/\/.*//')
+		elif (echo $link | grep -qi 'mangacow')
+		then
+			chp=$(echo  $link | sed -e "s/.*$name\///g" -e 's/\/.*//')
+		fi
+
+		#create a folder for images
+		if (! (ls | grep -q $chp))
+		then
+			mkdir $chp
+		fi
+
+		# download image
+		wget -U $userAgent -qP ./$chp/ $download 
+
+		# update urls
+		if (echo $link | grep -qiE '(mangahere|mangatown)')
+		then
+			tempLink=$link
+			link=$(wget -qO- -U $userAgent $link | grep next_page | tail -1 | grep -o http.*html)
+			if ( test -z "$link" )
+			then
+				link=$(wget -qO- -U $userAgent $tempLink | grep "Next Chapter" | sed -e 's/Previous.*//g' -e 's/.*href="//g' -e 's/\/".*//')
+			fi
+			download=$(wget -qO- -U $userAgent $link | grep .*.jpg | head -2 | tail -1 | grep -o http.*.jpg)
+		elif (echo $link | grep -qi 'mangapanda')
+		then
+			link=www.mangapanda.com$(wget -qO- $link | grep -oE href=\"/[0-9]*-*[0-9]*-*[0-9]*/*$name/[chapter-]*[0-9]*[.html]*/*[0-9]* | sed 's/href="//' | head -3 | tail -1)
+			if [ "$link" == "www.mangapanda.com" ]
+			then
+				link=""
+			fi
+			download=$(wget -qO- $link | grep -o .*jpg | sed 's/.*src=\"//' | tail -1 | grep $name)
+		elif (echo $link | grep -qi 'mangareader')
+		then
+			link=www.mangareader.net$(wget -qO- $link | grep -oE href=\"/[0-9]*-*[0-9]*-*[0-9]*/*$name/[chapter-]*[0-9]*[.html]*/*[0-9]* | sed 's/href="//' | head -3 | tail -1)
+			if [ "$link" == "www.mangareader.net" ]
+			then
+				link=""
+			fi
+			download=$(wget -qO- $link | grep -o .*jpg | sed 's/.*src=\"//' | tail -1 | grep $name)
+		elif (echo $link | grep -qi 'mangacow')
+		then
+			link=$(wget -qO- $link | sed 's/;/\n/g' | grep "wpm_nav_nxt =" | sed -e 's/.* = "//g' -e 's/\/"//g')
+			download=$(wget -qO- $link | grep -o .*jpg | sed 's/.*src=\"//' | tail -1)
+		fi
+
+		if test -z $(echo "$download" | grep ^h)
+		then
+			break
+		fi
+	done
+#===================================================== UPDATE ===================================================================
 else
 	awk -F ' ' '{print $1}' Links | while read mangaName
 	do
